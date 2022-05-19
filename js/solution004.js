d021a8bb442e58bbcd9b02d27a857948424c0e5c
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-05-19 12:46:20 -0400

    solution004 completed

---
+++ js/solution004.js
@@ -0,0 +1,12 @@
+//Instructions:
+//reverse a list order
+//example: [1,2,3,4] -> [4,3,2,1]
+
+/*function reverseList(list){
+    //code here
+}
+*/
+// my solution below:
+const reverseList = (list) => list.reverse()
+// i used const as my variable but it would still work with lets variable
+//arrow function elimate the return word and curly brackets so that the code fits in one line 
