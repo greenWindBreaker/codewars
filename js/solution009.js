7f4f65e818ffbce4aa55d7bb3833687720d5cb10
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-05-24 08:31:10 -0400

    mar 15 commit

--- js/solution009.js
+++ js/solution009.js
@@ -5,7 +5,6 @@ function nameShuffler(str){
     return String(str).split(' ').reverse().join(' ');
 }*///checks out, now to clean it
 
+const nameShuffler = str => String(str).split(' ').reverse().join(' ');
 
-
-const nameShuffler = str => String(str).split(' ').reverse().join(' ')
 console.log(nameShuffler('asuka langley'))// 'langley asuka'
---
+++ js/solution00a.js
@@ -0,0 +1,5 @@
+//Given an array of integers, return a new array with each value doubled.
+function maps(x){
+    return x.map(n=> n*2)
+}
+console.log(maps([2,6,8,5]))// [4,12,16,10]
