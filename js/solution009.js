cc74f33365dfff4acb55ed6923e187171128fe11
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-05-24 07:34:39 -0400

    solution009 completed

---
+++ js/solution009.js
@@ -0,0 +1,11 @@
+//Name shuffler
+//Write a function that returns a string in which firstname is swapped with last name.
+/*
+function nameShuffler(str){
+    return String(str).split(' ').reverse().join(' ');
+}*///checks out, now to clean it
+
+
+
+const nameShuffler = str => String(str).split(' ').reverse().join(' ')
+console.log(nameShuffler('asuka langley'))// 'langley asuka'
