0430700ab0f2a8c04284811e3ec4f1f5bce3dc78
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-05-25 09:14:10 -0400

    remove solution00a js

--- js/solution00a.js
+++
@@ -1,6 +0,0 @@
-//Given an array of integers, return a new array with each value doubled.
-function maps(x){
-    return x.map(n=> n*2)
-}
-console.log(maps([2,6,8,5]))// [4,12,16,10]
-console/log(maps([3,4,10]))//[6,8,20]
