1a1473fec0bc297882089110c08562becd8aa231
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-06-04 06:34:56 -0400

    solution019 completed

---
+++ js/solution019.js
@@ -0,0 +1,8 @@
+//solution019
+//Given an array of integers your solution should find the smallest integer
+//supplied array will not be empty
+class SmallestIntegerFinder {
+    findSmallestInt(args){
+        return Math.min(...args)
+    }
+}
