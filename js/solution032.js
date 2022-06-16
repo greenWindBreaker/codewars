8f94898b00913da026f3d56fe03ba139a0be6fa6
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-06-16 08:36:58 -0400

    solution032 updated

--- js/solution032.js
+++ js/solution032.js
@@ -1,1 +1,7 @@
-//solution032
+//solution032 updated
+//Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer
+//0 = 0
+
+const cockroachSpeed = s => s === 0 ? 0 : Math.floor(s*100000/3600)
+
+console.log(cockroachSpeed(1.08))//30
