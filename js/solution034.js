de4de459ec35f94af73e650546576ff8d02d1870
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-06-18 08:10:11 -0400

    solution034 updated

--- js/solution034.js
+++ js/solution034.js
@@ -1,1 +1,9 @@
 //solution034
+//Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+// the dish must start and end with the same letters as the animal's name.
+//Assume that beast and dish are always lowercase strings, and that each has at least two letters
+
+const feast = (beast,dish) => beast.charAt(0) === dish.charAt(0) && beast.charAt(beast.length-1) === dish.charAt(dish.length-1) ? true : false
+
+console.log(feast('bear','bread sugar'))//true
+console.log(feast('bear','bread'))//false
