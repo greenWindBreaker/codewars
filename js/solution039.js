fe111c2741fe7d46e903f77c178a20fb420c327d
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-06-20 07:53:57 -0400

    completed solution039

---
+++ js/solution039.js
@@ -0,0 +1,8 @@
+//solution039
+//When provided with a number between 0-9, return it in words.
+//Input :: 1
+//Output :: "One".
+
+const switchItUp = num =>['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine'][num]
+
+console.log(switchItUp(3))//'Three'
