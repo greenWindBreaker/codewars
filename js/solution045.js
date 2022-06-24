f9cb5b60228ca87ae2566c31b6bdc0baa7f7f2e9
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-06-24 07:35:44 -0400

    solution045 completed

---
+++ js/solution045.js
@@ -0,0 +1,7 @@
+//solution045
+//This function should test if the factor is a factor of base.
+//Return true if it is a factor or false if it is not.
+
+const checkForFactor = (base,factor) => base%factor === 0 ? true : false
+
+console.log(checkForFactor(10,2))//true
