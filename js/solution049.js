feffaa19cab0065ff6076539301e3048e59f45f8
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-06-26 08:08:22 -0400

    solution049 completed

---
+++ js/solution049.js
@@ -0,0 +1,6 @@
+//solution049
+//create a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+const sayHello = name => `Hello, ${name}`
+
+console.log(sayHello('Mr. Frog'))// Hello, Mr.Frog
