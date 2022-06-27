d8ad578bf4feb201f36523d4360e4a9b439dc830
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-06-27 08:29:32 -0400

    solution051 completed

---
+++ js/solution051.js
@@ -0,0 +1,7 @@
+//solution051
+//Your code will determine if the number passed is even (or not).
+//The function needs to return either a true or false.
+
+const testEven = x => x%2===0 
+
+console.log(testEven(2.01))// false
