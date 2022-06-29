a198a0bb80285f9625c1082266658a82d2627541
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-06-29 08:15:03 -0400

    solution054 completed

---
+++ js/solution054.js
@@ -0,0 +1,28 @@
+//solution054
+//Think of a way to store the languages as a database (eg an object). 
+//Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+
+function greet(language){
+    let x = {
+'english': 'Welcome',
+'czech': 'Vitejte',
+'danish': 'Velkomst',
+'dutch': 'Welkom',
+'estonian': 'Tere tulemast',
+'finnish': 'Tervetuloa',
+'flemish': 'Welgekomen',
+'french': 'Bienvenue',
+'german': 'Willkommen',
+'irish': 'Failte',
+'italian': 'Benvenuto',
+'latvian': 'Gaidits',
+'lithuanian': 'Laukiamas',
+'polish': 'Witamy',
+'spanish': 'Bienvenido',
+'swedish': 'Valkommen',
+'welsh': 'Croeso'
+    }
+    return x[language] || 'Welcome'
+}
+
+console.log(greet('finnish'))//'Tervetuloa'
