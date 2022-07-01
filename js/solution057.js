54c4adfac4933a5901985f51ed65497a0657317b
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-07-01 07:13:09 -0400

    solution057 completed

---
+++ js/solution057.js
@@ -0,0 +1,6 @@
+//solution057
+//Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string)
+
+const sumStr = (a,b) => String(Number(a)+Number(b))
+
+console.log(sumStr('2','4'))//'6'
