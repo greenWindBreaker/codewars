a305adc5c0c54a43cc1c70c4b9b9a9f8dc3b1309
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-07-14 07:09:17 -0400

    solution078 completed

---
+++ js/solution078.js
@@ -0,0 +1,12 @@
+//solution078
+//Write a function that produces an array with the numbers 0 to N-1 in it.
+
+function arr(n){
+    let newArr = []
+    for(let i = 0; i < n; i++){
+        newArr.push(i)
+    }
+    return newArr
+}
+
+console.log(arr(6))
