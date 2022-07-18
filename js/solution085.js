98443870abcb1370d4041528835af8d808be4921
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-07-18 05:36:37 -0400

    solution085 completed

---
+++ js/solution085.js
@@ -0,0 +1,7 @@
+//solution085
+//https://www.codewars.com/kata/56200d610758762fb0000002/solutions/javascript
+//I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number. Can you help me fix the function?
+
+const addFive = x => x + 5
+
+console.log(addFive(10))//15
