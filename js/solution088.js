a411269a7dbc1eb104ef8f4985400b2fd3064668
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-07-19 05:42:17 -0400

    solution088 completed

---
+++ js/solution088.js
@@ -0,0 +1,10 @@
+//solution088
+//A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+// function main [verb, noun]
+//     return verb + noun
+// }
+
+const main = (verb, noun) => `${verb}${noun}`
+
+console.log(main('chop ', 'tree'))// 'chop tree'
