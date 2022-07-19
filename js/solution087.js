90ab3589f8300d4c760bc465d630887ca7406884
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-07-19 05:36:03 -0400

    solution087 completed

---
+++ js/solution087.js
@@ -0,0 +1,6 @@
+//solution087
+//Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+const take = (arr,n) => arr.slice(0, n)
+
+console.log(take([3,6,1,9,45,361,8],4))//[3,6,1,9]
