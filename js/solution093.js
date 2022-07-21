5d4c28369bd863d9463f7d89741277ca1e268a5d
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-07-21 06:40:19 -0400

    solution093 completed

---
+++ js/solution093.js
@@ -0,0 +1,14 @@
+//solution093
+//Write a function isReallyNaN that returns true only if passed in an argument that evalutes to NaN, and returns false otherwise.
+//https://www.codewars.com/kata/56c24c58e0c0f741d4001aef/solutions/javascript
+
+const isReallyNaN = val => val !== val
+
+console.log(isReallyNaN(90))
+console.log(isReallyNaN('90'))
+console.log(isReallyNaN('ten'))
+console.log(isReallyNaN(null))
+console.log(isReallyNaN(undefined))
+console.log(isReallyNaN(false))
+console.log(isReallyNaN(true))
+console.log(isReallyNaN(0))
