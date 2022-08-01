bce52e59e9bb833c0b9f569a39eb83e6e0ac49ed
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-08-01 08:46:49 -0400

    solution105 completed

---
+++ js/solution105.js
@@ -0,0 +1,12 @@
+//solution105
+//KEEP HYDRATED...You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+//For example:
+//time = 3 ----> litres = 1
+//time = 6.7---> litres = 3
+//time = 11.8--> litres = 5
+
+function litres(time){ 
+    return Math.trunc(time * 0.5);
+}
+
+console.log(litres(15))// 7
