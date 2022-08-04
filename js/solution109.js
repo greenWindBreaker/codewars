959b6b5deaff4177c7d3c0e08f5506e2d6e08283
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-08-04 06:57:54 -0400

    solution109 completed

---
+++ js/solution109.js
@@ -0,0 +1,15 @@
+//solution109
+//This is the first step to understanding FizzBuzz.git add .
+//Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+//Your expected output is an array of positive integers from 1 to n (inclusive).
+//Your job is to write an algorithm that gets you from the input to the output.
+
+function preFizz(n){
+    let newArray = []
+    for(let i =1; i<=n; i++){
+        newArray.push(i)
+    }
+    return newArray
+}
+
+console.log(preFizz(25))//[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
