2548f5f660fdc5acd2f7def91a8981662bd57c0b
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-08-04 06:48:50 -0400

    solution108 completed

---
+++ js/solution108.js
@@ -0,0 +1,11 @@
+//solution108
+//Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+//var a == 'dev'
+//var b == 'Lab'
+//var name == a+b
+
+let a = 'dev' // i fixed the code by removing the extra = sign for all the variables
+let b = 'Lab' // i replaced all var with let
+let name = a+b
+
+console.log(name)// devLab 
