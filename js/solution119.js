fbfcbc8646fc32258ccec714620ac8aad743405c
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-08-12 06:06:08 -0400

    solution119 completed

---
+++ js/solution119.js
@@ -0,0 +1,7 @@
+//solution119
+//Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+//Your task is to return the correct string using the Template String Feature.
+
+const TempleStrings = (obj,feature) => `${obj} are ${feature}`
+
+console.log(TempleStrings('smartphones','neat'))//'smartphones are neat'
