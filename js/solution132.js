3b9ac0be72ad5e2a653fba8c7f24d6861e16d496
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-08-23 09:18:21 -0400

    solution132 completed

---
+++ js/solution132.js
@@ -0,0 +1,20 @@
+//solution132
+//Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+//example: 'aabacbaa' --> 'bbabcabb'
+
+function switcheroo(x){
+    //return x.replaceAll('a','b')//works but not what we want
+    let newArr = []
+    for(let i =0; i < x.length ; i++){
+        if(x[i]==='a'){
+            newArr.push('b')
+        }else if(x[i] === 'b'){
+            newArr.push('a')
+        }else{
+            newArr.push(x[i])
+        }
+    }
+    return String(newArr).replaceAll(',','')//String(newArr) will return 'b,b,c,c,a,a' string with commas. so to fix that we use replaceAll(',','') which removes all commas. the end result is 'bbccaa' 
+}
+
+console.log(switcheroo('aaccbbaaddba'))//'bbccaabbddab'
