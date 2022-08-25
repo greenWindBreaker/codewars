c98a9a905d0128c091c016a9b0ce102c6d438973
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-08-25 06:08:43 -0400

    solution135 completed

---
+++ js/solution135.js
@@ -0,0 +1,6 @@
+//solution135
+//ES6 has introduced another way of joining strings. Your task is to find out what this is and write a function that will add two strings together, there must be a space between the two strings.
+
+const joinStrings = (str1,str2) => `${str1} ${str2}`
+
+console.log(joinStrings('mon','day'))// mon day
