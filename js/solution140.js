7a53b959be8dcceff0e28692a4cdc2c63d48cf6e
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-08-30 06:18:52 -0400

    solution141 completed

--- js/solution140.js
+++ js/solution140.js
@@ -1,4 +1,5 @@
 //solution140
+//You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
 
 function fireFights(s){
     let divvy = s.split(' ')
