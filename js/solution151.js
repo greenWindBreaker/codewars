54b214f0bf9fbeb8e3b4c83b4e57f22618a51f55
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-09-07 08:08:18 -0400

    solution151 completed

---
+++ js/solution151.js
@@ -0,0 +1,7 @@
+//solution151
+
+//Add the value "codewars" to the websites array.
+//After your code executes the websites array should == ["codewars"]
+//The websites array has already been defined for you using the following code: var websites = []
+
+websites.push('codewars')// this will modify the existing array instead of making a new array. if we wanted to make a new array then we would write-> websites = ['codewars'] 
