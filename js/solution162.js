35e45f6c355ad85047ab4047829cd1766eadf284
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-09-17 17:28:10 -0400

    solution162 completed

---
+++ js/solution162.js
@@ -0,0 +1,8 @@
+//solution162
+//SENTENCE SMASH.. join array of words together into one string
+
+function smash(words){
+    return words.join(' ');
+}
+
+console.log(smash(['cow','green','tile']))// 'cow green tile'
