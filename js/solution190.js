c40fc3fee26c8ff30de285c09b9e8684dd7b26c9
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-10-18 08:17:28 -0400

    solution190 completed

---
+++ js/solution190.js
@@ -0,0 +1,9 @@
+//solution190
+//Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+//ex: "www.codewars.com#about" --> "www.codewars.com"
+//ex: "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+const removeUrlAnchor = url => url.replace('#',' ').split(' ')[0]
+
+console.log(removeUrlAnchor("www.codewars.com#about"))//checks out
+console.log(removeUrlAnchor('www.codewars.com?page=1'))//checks out
