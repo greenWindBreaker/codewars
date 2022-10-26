818d21a4a9e1aa8a9950d6905571b58d354b253f
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-10-26 18:32:35 -0400

    standup008 attended

---
+++ js/standup008.js
@@ -0,0 +1,14 @@
+//standup008
+//given an array of integers, fins the Nth smallest element in this array of integers
+//notes: array/list size is at least 3
+//arrays/lists numbers could be a mixture of positives, negatives, and zeros.
+//repetition in array/lists numbers could occur, so dont remove duplications.
+
+// nthSmallest({3,1,2} ,2) ==> return (2) 
+// nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
+// nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
+// nthSmallest({177,225,243,-169,-12,-5,2,92} ,5) ==> return (92) 
+
+function nthSmallest(arr){
+    //sort method
+}
