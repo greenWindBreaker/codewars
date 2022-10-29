1bea91a640ad74615f4c6918d555f94f486c15e9
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-10-29 14:54:05 -0400

    solution198 completed

---
+++ js/solution198.js
@@ -0,0 +1,6 @@
+//solution198
+//BEGINNER SERIES #1 SCHOOL PAPERWORK... Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages. Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0. ex: n=5,m=5 -> 25, n=-5,m=5-> 0
+
+const paperwork = (n,m) => n < 0 || m < 0 ? 0 : n * m
+
+console.log(paperwork(5,5))//25
