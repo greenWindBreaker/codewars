7b009aee33089ab2f84361e85879b54ffe7e9519
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-11-01 18:28:33 -0400

    standup010 attended

---
+++ js/standup010.js
@@ -0,0 +1,27 @@
+//standup010
+// Given a string, return a new string that has transformed based on the input:
+
+// Change case of every character, ie. lower case to upper case, upper case to lower case.
+// Reverse the order of words from the input.
+// Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+// For example:
+
+// "Example Input" ==> "iNPUT eXAMPLE"
+// You may assume the input only contain English alphabet and spaces.
+
+//function transformStr(str){
+    //split str " " -> reverse -> join
+    //split str " " -> map l === l.toUpperCase() ? l.toLowerCase() : l.toUpperCase()
+    //l = letter
+    //let reversedStr = str.split(' ').reverse().join(' ')
+    //let transformedStr = reversedStr.split('').map(l => l.toUpperCase() ? l.toLowerCase() : l.toUpperCase()).join('')// map is looping through each letter , complict return, implicent return 
+    //return transformStr
+    //one line solution beloew:
+    //return str.split(' ').reverse().join(' ').split('').map(l => l.toUpperCase() ? l.toLowerCase() : l.toUpperCase()).join('')
+//}
+
+const transformedStr = str => str.split(' ').reverse().join(' ').split('').map(l => l.toUpperCase() ? l.toLowerCase() : l.toUpperCase()).join('')
+
+console.log(transformStr('Example Input'))// 
+console.log(transformStr('Frosty Dog'))// 
