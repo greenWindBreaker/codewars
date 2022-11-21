55a50a55858f1f5d4edeba6ba7a61268d6c3e723
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-11-21 18:13:58 -0500

    standup019 attended

--- js/standup019.js
+++ js/standup019.js
@@ -1,2 +1,13 @@
 //standup019
 //for monday, 11/21/22
+// Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta"
+
+function removeDupeWords(str){
+    //remove something from something -> usually will use filter()
+    //will take a str, and split(" ") and then filter()
+    return str.split(' ').filter((w,i,a)=> w != a[i-1]).join(' ')
+}
