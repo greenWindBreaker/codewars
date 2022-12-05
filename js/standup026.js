6d7741d2147eb0a1d722ca2786cca9f7859e4be3
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-12-05 15:03:25 -0500

    prep for standup026

---
+++ js/standup026.js
@@ -0,0 +1,5 @@
+//standup026
+// 12/05/2022 Monday @ 6pm on remo.co  
+// Count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
