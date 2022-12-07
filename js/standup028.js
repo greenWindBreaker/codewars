3e6ebd2bfda9a55532fd639c7a2731d9c36b2b61
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-12-07 18:20:42 -0500

    added notes

--- js/standup028.js
+++ js/standup028.js
@@ -1,2 +1,14 @@
 //standup028
 // wednesday 12/07/2022, @ 6pm remo.co  
+// Given a string of characters, return the character that appears the most often.
+
+// describe("Max Character", () => {
+//  it("Should return max character", () => {
+//   assert.equal(max("Hello World!"), "l");
+//  });
+// });
+
+// No String or Array Methods (well brute force it first, but then no methods)! 
+function mostOften(str){
+
+}
