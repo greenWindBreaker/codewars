bb88b847520134625b06cafcea4f90e64bdee391
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-12-12 18:30:07 -0500

    standup029 attended

--- js/standup029.js
+++ js/standup029.js
@@ -1,2 +1,29 @@
 //standup029
 // Monday 12/12/2022 @6pm remo.co 
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+function containsDupes(arr){
+    let numsMap = {}
+    for(let i = 0; i < arr.length; i++){
+        let num = arr[i]
+        if(numsMap[num]){
+            return true
+        }else{
+            numsMap[num] = true
+        }
+    }
+    return false
+}
