14c7df86254f29d5d15a0e92e15c4bcb915567e5
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-12-27 10:48:26 -0500

    changes

--- js/standup037.js
+++ js/standup037.js
@@ -1,4 +1,5 @@
 //standup037
 // Jan 2nd, @ 6pm est on remo.co
 //Merry Christmas Push 🎄
-//12/26/2022 push
+// 12/26/2022 push
+// just looked at calendar and jan 2nd is a holiday so next standup probably on Jan 3rd 2023 
