0a39a27e1f3b3b8a229d7dc66cb44c95a606bd00
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-01-22 18:55:37 -0500

    standup051 preparation

--- js/standup002.js
+++ js/standup002.js
@@ -16,14 +16,15 @@ console.log(getAverage([1,2,3,4,5]))//3
 const repeats = arr => arr.filter((n)=> arr.indexOf(n) === arr.lastIndexOf(n)).reduce((a,c)=> a+c,0)
 console.log(repeats([4,5,7,5,4,8]))//15
 //---------------------------
-// Given an array of digital numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
-// For example:
-// ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
-// ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
-// ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
-function myFunc(arr,x){
-    //return arr.filter((e)=> e%2 ===0);//filters out all non even numbers and returns a copy of the arr with only even nums in it
-    return arr.filter((e)=> e%2 ===0).slice(-x);
-}
+const myFunc=(arr,x)=>arr.filter(e=>e%2===0).slice(-x);
 console.log(myFunc([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))// [4,6,8]
 console.log(myFunc([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))// [-8,26]
+//---------------------------
+
+// Return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+// For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+// Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+// solve([[1,2],[4],[5,6]]),4)
+// solve([[1,2],[4,4],[5,6,6]]),4)
+// solve([[1,2],[3,4],[5,6]]),8)
+// solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72)
---
+++ js/standup051.js
@@ -0,0 +1,2 @@
+//standup051
+// Monday, 01/23/2023, standup at 6pm on remo.co
