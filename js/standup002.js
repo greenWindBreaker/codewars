2d3ed6c7d4307e898f77f8bef5213a5fdad7580b
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-01-23 18:55:15 -0500

    standup051 attended

--- js/standup002.js
+++ js/standup002.js
@@ -20,11 +20,18 @@ const myFunc=(arr,x)=>arr.filter(e=>e%2===0).slice(-x);
 console.log(myFunc([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))// [4,6,8]
 console.log(myFunc([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))// [-8,26]
 //---------------------------
-
 // Return the number of unique arrays that can be formed by picking exactly one element from each subarray.
-// For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
-// Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
-// solve([[1,2],[4],[5,6]]),4)
-// solve([[1,2],[4,4],[5,6,6]]),4)
-// solve([[1,2],[3,4],[5,6]]),8)
-// solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72)
+function solve(arr){
+    return arr.map(e=> new Set(e).size).reduce((a,c)=> a*c,1)
+}
+console.log(solve([[1,2],[4,4],[5,6,6]]))//[1,2],[4],[5,6]]
+//------------------------------
+// Given an array of integers , Find the Nth smallest element in this array of integers.
+// Notes
+// Array/list size is at least 3 .
+// Array/list's numbers could be a mixture of positives , negatives and zeros .
+// Repetition in array/list's numbers could occur , so don't Remove Duplications .
+// nthSmallest({3,1,2} ,2) ==> return (2) 
+// nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
+// nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
+// nthSmallest({177,225,243,-169,-12,-5,2,92} ,5) ==> return (92) 
