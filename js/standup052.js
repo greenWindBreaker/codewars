54e901c2c76d529b6b549b41d3c66fbc7787aa94
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-01-30 18:34:34 -0500

    standup052 attended

--- js/standup052.js
+++ js/standup052.js
@@ -1,2 +1,3 @@
 //standup052
-//Monday, 01/30/23, standup @ 6pm on remo.co
+//Monday, 01/30/23, standup @ 6pm on discord
+//today we had a Q&A session, tmrw we will finish up on Data Structures & Algorithms! 
