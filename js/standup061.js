c3e2d9604cb135be94de0621bd6476633bcc64f3
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-02-13 19:29:24 -0500

    standup061 attended

--- js/standup061.js
+++ js/standup061.js
@@ -1,2 +1,4 @@
 //standup061
 //Monday 02/13/23 standup on remo.co @ 6pm
+//Standup at 6pm ET (~2 hours from now)! We're starting our dive into Linked Lists. See you soon! -Leon
+//talk about Linked List and had Q&A session, no code
