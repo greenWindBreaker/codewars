c23b9dba748635d75d35a51796dab2e778046a17
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-02-17 22:51:25 -0500

    standup065 changes

--- js/standup065.js
+++ js/standup065.js
@@ -1,3 +1,4 @@
 //standup065
 //Teaspill on discord @ 6pm 
-//notes: writing a technical blog about hard concepts can make you a thought leader 
+//notes: writing a technical blog about hard concepts can make you a thought leader
+//it might help you stand out  
