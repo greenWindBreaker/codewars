59a4c444da6f722cd0a2d4b5e6a59df096b11569
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-02-19 11:56:47 -0500

    standup066 changes

--- js/standup002.js
+++ js/standup002.js
@@ -8,10 +8,7 @@ console.log(parseDomainName('https://github.com/asesome-project/username'))//'gi
 console.log(parseDomainName('https://www.xombie-bites.com'))//xombie-bites
 console.log(parseDomainName('https://www.cnet.com'))//cnet
 
-//---------------------------
-const getAverage=arr=>Math.floor(arr.reduce((a,c)=> a + c,0)/arr.length);
-console.log(getAverage([2,2,2,2]))//2
-console.log(getAverage([1,2,3,4,5]))//3
+
 //--------------------------
 console.log("starts here:")
 //You will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
@@ -23,7 +20,6 @@ console.log(repeats([4,5,7,5,4,8]))//15
 //---------------------------
 //Given an array of digital numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
 function myFunc(arr,x){
- 
 }
 console.log(myFunc([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))// [4,6,8]
 console.log(myFunc([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))// [-8,26]
@@ -34,14 +30,14 @@ console.log(myFunc([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))// [-8,26]
 // solve([[1,2],[4],[5,6]]),4)
 // solve([[1,2],[4,4],[5,6,6]]),4)
 function solve(arr){
-  
+
 }
 console.log(solve([[1,2],[4,4],[5,6,6]]))//4
 console.log(solve([[1,2],[4,4,6],[5,7,6]]))//2*2*4=12
 //------------------------------
 //Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each. standup011
 function last(str){
-    
+   
 }
 console.log(last('man i need a taxi up to ubud'))// ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
 console.log(last('the wheels on the bus go round n round'))//['round', 'round', 'the', 'the', 'on', 'n', 'go', 'wheels', 'bus']
--- js/standup066.js
+++ js/standup066.js
@@ -1,3 +1,4 @@
 //standup066
 //Tuesday, 02/19/2023, standup on remo.co @ 6pm
 //Monday was a holiday so no standup
+//notes:
