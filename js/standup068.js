e6c29d2096b771ace46cbe9c55dc689299a780d5
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-02-23 18:39:57 -0500

    notes added

--- js/standup068.js
+++ js/standup068.js
@@ -1,3 +1,12 @@
 //standup068
 //Thursday 02/23/22, standup on remo.co @ 6pm 
 //was suppose to do linkedlist today but Leon moved it to next week, today will be Q&A session
+//Work the hardest you will ever work during the 1st 3 months of you software engineer career - Leon
+//ask every question possible 
+//know the stack 
+//work really hard in day 
+//work really hard in night
+//communicate communicate communicate
+//know the framework, consume all content related to the tech your working with
+//networking events, never eat alone-> friendships made when guard is down. 
+//townhall tmrw 
