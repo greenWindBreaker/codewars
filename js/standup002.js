a01533802d127e9b8b380b75e40007e1e9fea7f1
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-02-27 18:39:26 -0500

    standup070 attended

--- js/standup002.js
+++ js/standup002.js
@@ -9,7 +9,19 @@ console.log(parseDomainName('https://www.xombie-bites.com'))//xombie-bites
 console.log(parseDomainName('https://www.cnet.com'))//cnet
 
 function fizzBuzz(num){
-    
+    let result = []
+    for(let i = 1;i<=num;i++){
+        if(i%5===0 && i%3===0){
+            result.push('fizzbuzz')
+        }else if(i%5===0){
+            result.push('buzz')
+        }else if(i%3===0){
+            result.push('fizz')
+        }else{
+            result.push(i)
+        }
+    }
+    return result
 }
 console.log(fizzBuzz(50))
 //--------------------------
@@ -17,13 +29,13 @@ console.log("starts here:")
 //You will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
 //For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
 function repeats(arr){
-    
+    return arr.filter(x=>arr.indexOf(x)===arr.lastIndexOf(x)).reduce((a,c)=>a+c,0)
 }
 console.log(repeats([4,5,7,5,4,8]))//15
 //---------------------------
 //Given an array of digital numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
 function myFunc(arr,x){
-  
+    return arr.filter(x=>x%2===0).slice(-x)
 }
 console.log(myFunc([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))// [4,6,8]
 console.log(myFunc([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))// [-8,26]
@@ -34,14 +46,14 @@ console.log(myFunc([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))// [-8,26]
 // solve([[1,2],[4],[5,6]]),4)
 // solve([[1,2],[4,4],[5,6,6]]),4)
 function solve(arr){
-   
+    return arr.reduce((a,c)=> a * new Set(c).size,1);
 }
 console.log(solve([[1,2],[4,4],[5,6,6]]))//4
 console.log(solve([[1,2],[4,4,6],[5,7,6]]))//2*1, 2*2=4, 4*3=12
 //------------------------------
 //Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each. standup011
 function last(str){
-   
+    return str.split(' ').sort((a,b)=>a.charCodeAt(a.length-1)-b.charCodeAt(b.length-1));
 }
 console.log(last('man i need a taxi up to ubud'))// ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
 console.log(last('the wheels on the bus go round n round'))//['round', 'round', 'the', 'the', 'on', 'n', 'go', 'wheels', 'bus']
--- js/standup070.js
+++ js/standup070.js
@@ -1,2 +1,19 @@
 //standup070
 //Monday 02.27.23, standup on remo.co @ 6pm 
+//LinkedList
+//nodes, head and tail. nodes contain a pointer to the next node.
+//double linked list -> a pointer to net node and previous node
+//nodes have a value and pointer - singlely linklist
+//singlely linkedlist -> head and tail: the tail points to null, the head points to next
+//challenge below:
+//Given the head of a linked list, return the middle node of the linked list.If there are two middle nodes, return the second middle node.
+//Hint:  Remember our nodes have a next property. Loop through all the nodes at two speeds .next and .next.next by the time the second speed reaches the end the first will be at the middle. Just return what node it is currently at
+//fast and slow pointer method- each time i loop one pointer go 2x speed, one pointer go normal speed
+var middleNode = function(head){
+    slow = fast = head;
+    while(fast && fast.next){
+        slow=slow.next;
+        fast=fast.next.next;//it will break when its at the tail, because tail is null
+    }
+    return slow;
+}
