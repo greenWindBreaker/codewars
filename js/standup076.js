eb5b5abbac2a6982d2626aec1efe164c5efec526
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-03-07 18:21:18 -0500

    standup076 attended

--- js/standup076.js
+++ js/standup076.js
@@ -1,2 +1,14 @@
 //standup076
 //Tuesday, 03-07-23, standup on remo co @ 6pm 
+//graphs and trees
+//linkedlist made up of nodes, each node has a value and a pointer. value can = anything. each pointer has a 'next' pointer.
+//graphs are nodes that are connected to each other
+//idea: large node based structure, delviery apps use graph
+//this week we will learn about Trees
+//they expand in one direction. think of an upside down tree. the fist node is called the 'root node'
+//pair and child relationships
+//you can have one parent node that branch out to many children node
+//heaps keep stuff in order, good for holding words
+//binary trees: each node has a max of 2 pointers. left and right pointer. becomes the basis of a lot of common problems in job hunt. 
+//file systems depend on tree systems.
+//the DOM is a node structure, its a tree like structure <div - parent > <p-child ></p> <p- sibling ></p> </div> 
