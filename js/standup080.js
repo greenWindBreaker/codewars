57a9450144f088d3dedb8b987f9cd4be60c424b9
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-03-13 18:33:27 -0400

    standup080 attended

--- js/standup080.js
+++ js/standup080.js
@@ -1,3 +1,7 @@
 //standup080
 //Monday, 03-13-23, standup on remo.co @ 6pm
-//notes
+//Q&A session today, tmrw Trees
+//notes:
+//i recorded a voice memo on phone titled, 'standup.3.13.23'
+//be on the marketing team, where you are the god that can code - Leon
+//look job board in area and go on that website
