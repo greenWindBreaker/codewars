fde69c3378d11be1696bddac8a9675954dafabf2
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-03-20 18:49:48 -0400

    standup085 changes

--- js/standup085.js
+++ js/standup085.js
@@ -2,3 +2,9 @@
 //Monday, 03-20-23, standup on Discord @ 6pm   
 //Q&A session today
 //notes:
+//48in48 for volentury work hack a thon
+//apporaching a loacl nonprofit, the point of free work is for reference so taht they sing your praise
+//make sure to focus on the hunt, bacni/anki/ codewars/ hotlist 
+//triple down on networking, sending messages, reaching out to folxs with open positions
+//1st paid client = 1st professional software engineer experience
+//@100devs you are a freelancer and not need a w2 make sure they understand that, show client work as proof
