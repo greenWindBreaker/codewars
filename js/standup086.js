79a2b3ac1fc1df51b229f25f57703e0c7db7bf91
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-03-21 18:42:58 -0400

    standup086 attended

---
+++ js/standup086.js
@@ -0,0 +1,12 @@
+//standup086
+//Tuesday, 03-21-23, standup Q&A session on Discord 
+//last standup until friday -> 3-24-23
+//notes:
+//job posting for software engineer have increased recently
+//build a stopwatch using React's state, get it it functioning = start, stop, reset, no decimal for seconds, 
+//if you dont finish an assignment - you should finish it and send it to them
+//networking-> talkingg to folx is practice and take it seriously, go to events, local events, talk to real ppl, talk to engineers, the 1st 20 times it will go horrible but you will prevail
+//banki (prep behavioral) anki, hitlist (messaging, events, reaching out), codewars, projects -> the order = hitlist, bank anki, codewars, projects and learning react/java
+//plan out days and weeks
+//as long as you are kind and affirming , youll be okay
+//very few ppl check for references, like 80% 
