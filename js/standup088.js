d6036f74f23f426ce8dbc1c6e12197b4e19b3afb
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-03-27 18:59:09 -0400

    standup088 attended

--- js/standup088.js
+++ js/standup088.js
@@ -1,2 +1,6 @@
 //standup088
-//Monday, 03-27-23, standup on remo.co @ 6pm
+//Monday, 03-27-23, standup on discord @ 6pm
+//Q&A session
+//notes:
+//must learn to use AI, it will make you a better developer 
+//2 things on 1st job and 1 st year as developer-> getting to know teammates = making friends in the industry will help forever, and the code.
