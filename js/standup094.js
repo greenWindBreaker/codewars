fe78cea555e580caf37178e73f9f4293417d11d6
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-04-04 18:58:54 -0400

    standup094 attended

---
+++ js/standup094.js
@@ -0,0 +1,9 @@
+//standup094
+//Tuesday, 04-04-23, standup on discord @ 6pm
+//notes:
+//screenshots for making games with api logic, good to know not a priority
+//do not fly to conferences, to talk to someeone for a chance at getting a job - Leon
+//the hunt: sending messages is a full time endevor -Leon
+//Always know a lot about the company -Leon (star)
+//capitalism is intrinsically exploitive, they use your labor to make profit, and would pay you peanuts if they could, i will get mine no matter what, fuck them  ~ Leon
+//wont stop, wont stop, wont stop the beat! cant stop, cant stop, cant the beat! GO!... Everybody! Move your feet and feel united! WOOOAHH - junior senior
