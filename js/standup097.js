8ab1872eccd7afebfb640c39ab3d6549a3e5db98
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-04-07 18:42:34 -0400

    standup097 attended

---
+++ js/standup097.js
@@ -0,0 +1,5 @@
+//standup097
+//Friday, 04-07-23, standup on discord @ 6pm 
+//notes:
+//THE WINDOW  for software engineer has been open for 10 years and now the window just got bigger - Leon
+//more jobs less competition 2023 and beyond - leon
