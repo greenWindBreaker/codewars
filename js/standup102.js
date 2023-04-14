d89601b1c707730071766ba11604a6dcd508a7c6
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-04-14 18:49:52 -0400

    standup102 attended

---
+++ js/standup102.js
@@ -0,0 +1,8 @@
+//standup102
+//Friday, 04-14-23, standup on discord @ 6pm 
+//notes:
+//do not say your want mentor ship and leanr, you come off as junior -Leon
+//present yourself as a software engineer that can kick butt on day 1
+//message people monday-thursday but not on fridays 
+//next thursday there will be a 100 devs event on discord for 100devs to mingle
+//in meetups introduce yourself as a freelancer
