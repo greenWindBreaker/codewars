9e0dc4f16e588df84cab0a16645652396dccc874
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-05-01 18:53:55 -0400

    standup113 completed

--- js/standup113.js
+++ js/standup113.js
@@ -1,1 +1,7 @@
 //standup113
+//Monday, 05-01-23, Standup on discord @ 6pm
+//notes:
+// getting back on the hunt-> send messages and coffee chat requests
+//go to where the software engineers are, events, meetup.com start there
+//do the ai course from leon, computer vision project machine learning project worth it - leon
+//for products-> use shopify with a shopify template -Leon
