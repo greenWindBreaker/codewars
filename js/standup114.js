12b2e6f098319ee22a8e0dd3c7f59901e10931cf
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-05-02 19:21:46 -0400

    standup114 completed

--- js/standup114.js
+++ js/standup114.js
@@ -1,2 +1,5 @@
 //standup114
 //Tuesday, 05-02-23, standup Q&A on discord @ 6pm, wednesday,thursday off
+//notes:
+//colleges dont teach web development which makes getting a job for ppl without degrees accessible - Leon
+//no sign of slowing down of web dev hires - Leon
