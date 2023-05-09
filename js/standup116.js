9c47a9eb357af240688a730172f76ba3731a7435
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-05-09 19:05:47 -0400

    standup117 completed

--- js/standup116.js
+++ js/standup116.js
@@ -1,9 +1,3 @@
 //standup116
 //Monday, 05-08-23, standup on discord @ 6pm
 //leon cancelled
-
-function abbrevName(name){
-    let arr = name.split(' ')
-    return arr[0][0]+'.'+arr[1][0]
-}
-console.log(abbrevName('Micky Mouse'))
