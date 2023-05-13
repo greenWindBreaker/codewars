b273f3cde339be91fc3ed705de4efc9fbdc18b4f
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-05-13 13:57:16 -0400

    changes

--- js/standup119.js
+++ js/standup119.js
@@ -1,1 +1,5 @@
 //standup119
+//Monday, 05-13-23, standup on discord @ 6pm
+
+const doubleChar=str=>str.split('').map(x=>x+x).join('')   
+console.log(doubleChar("micky mouse"))//mmiicckkyy  mmoouussee
