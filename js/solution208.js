c0775632b84f857648dbf3340cbbb91c857de588
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-05-14 19:20:14 -0400

    solution208 completed

---
+++ js/solution208.js
@@ -0,0 +1,9 @@
+//solution208
+// return the user's correct age as an integer. Happy coding :) 
+// function getAge(inputString){
+//     return Number(inputString[0])
+// }
+// const getAge=s=>Number(s[0])
+const getAge=parseInt
+console.log(getAge('4 years old'))//4
+console.log(4)
--- js/standup119.js
+++ js/standup119.js
@@ -1,5 +1,5 @@
 //standup119
-//Monday, 05-13-23, standup on discord @ 6pm
+//Monday, 05-15-23, standup on discord @ 6pm
 
 const doubleChar=str=>str.split('').map(x=>x+x).join('')   
 console.log(doubleChar("micky mouse"))//mmiicckkyy  mmoouussee
