88b23882faaad0744296f8be96032b8c36e2ec6d
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-05-16 11:42:12 -0400

    standup120 preparation

--- js/standup119.js
+++ js/standup119.js
@@ -2,8 +2,17 @@
 //Monday, 05-15-23, standup on discord @ 6pm
 //looking for a new challenge  new oppurenity, never answer with 'im in it for bigger paycheck"
 function findDifference(a,b){
-    return a.reduce((a,c)=>a*c,1) 
+    return Math.abs(a.reduce((a,c)=>a*c,1)-b.reduce((a,c)=>a*c,1))
 }
 console.log(findDifference([2,2,3],[5,4,1]))//true
-console.log(findDifference())//false
-console.log(findDifference())//true
+
+
+function lol(){
+    a=3
+    b=4
+    c=a+b
+    a=2
+    d=a-1
+    console.log(c+d)
+}
+console.log(lol())
