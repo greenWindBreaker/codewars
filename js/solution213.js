b4e8aba5e012eaa6aa339d623c4a8ea12d7d4f4e
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-06-04 18:37:49 -0400

    changes

--- js/solution213.js
+++ js/solution213.js
@@ -1,6 +1,10 @@
 //solution213
 function fizzbuzz(x){
-    
-    return `place holder, your num is ${x}`
+    for(let i=0;i<=x;i++){
+        if(i%3===0 && i%5===0){
+            console.log('fizzbuzz')
+        }
+    }
+    //return `place holder, your num is ${x}`
 }
 console.log(fizzbuzz(100))
