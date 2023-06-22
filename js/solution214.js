ebaa51070c787a89f521e6a7f37b9791ab777ba2
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-06-21 20:10:14 -0400

    changes

--- js/solution214.js
+++ js/solution214.js
@@ -2,4 +2,4 @@
 function ave(x){
     return (x.reduce(el=>el+el)/x.length)*.8
 }
-console.log(ave([369,450,236,359,222,978,78,612,600,49,12,888]))
+console.log(ave([369,450,236,359,222,978,78,612,600,49,12,888,1389]))
